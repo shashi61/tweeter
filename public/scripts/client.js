e24d3f9dc9488c7ded9e18ef2064f26899c37c32
Shashi Bawa <shashibawa104@gmail.com>
2021-12-16 19:44:20 +0000

    implemented getch data from server functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,22 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $('document').ready(()=>{
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": timeago.format(new Date())
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": timeago.format(new Date())
-    }
-  ]
-
 const createTweetElement = function(tweet) {
+    // const $avatar = $('<img />').attr("src", `${tweet.user.avatars}`); 
+    // const $name = $('<span>').text(`${tweet.user.name}`);
+    // const $handle= $('<span>').attr("id","user-email").text(`${tweet.user.handle}`);
+    // const $content = $('<p>').text(`${tweet.content.text}`);
+    // const createdTime = $('<span>').text(`${tweet.created_at}`);
+    // const hr = $('<hr />');
+
+    // const article = $('<article>');
+    // const header = $('<header>');
+    // const footer = $('<footer>');
+
+    // const divHeader = $('<div>');
+    // const divMain = $('<div>');
+    // const divFooter = $('<div><i class="fa-solid fa-flag"></i><i class="fas fa-retweet"></i><i class="fa-solid fa-heart"></i> </div>');
+
   let $tweet = $(`<article>
   <header>
   <img src= ${tweet.user.avatars}>
@@ -55,13 +45,12 @@ const createTweetElement = function(tweet) {
 }
 const renderTweets = function(tweets) {
   // loops through tweets
-  for(let data of tweets){
+  for(let tweet of tweets){
     // calls createTweetElement for each tweet 
-    $('#tweets-container').append(createTweetElement(data)); 
+    $('#tweets-container').append(createTweetElement(tweet)); 
   }
   
 }
-renderTweets(data);
 const form = $('.form');
 //submit event for the form
 form.on("submit", function(event){
@@ -75,4 +64,21 @@ form.on("submit", function(event){
           console.log(res);
       })  
 })
+
+//Function to load tweets
+const loadTweets= function(){
+    $.ajax({
+        url: "/tweets",
+        method: "GET",
+        data: "json",
+        success: (tweets) => {
+            console.log('data', tweets);
+            renderTweets(tweets);
+        }, 
+        error: (err) =>{
+            console.log('err', err);
+        }
+    })
+}
+loadTweets();
 })
