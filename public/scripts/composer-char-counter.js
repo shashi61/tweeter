4e8fd6dfcdfcb2175004c77ef69bf583169817c3
Shashi Bawa <shashibawa104@gmail.com>
2021-12-17 03:33:33 +0000

    Added comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,25 @@
-//Wraping all the code in a callback function to ensure Html has loaded 
+//Wraping all the code in a callback function to ensure Html has loaded
 $('document').ready(()=>{
 //finding and saving text-area element to a variable
-const myInput = $('#tweet-text');
-//Finding and saving the output element to a variable
-const counter = $('.counter');
-const max = 140;
-//registering a call back 
-myInput.on("input", function(){
+  const myInput = $('#tweet-text');
+  //Finding and saving the output element to a variable
+  const counter = $('.counter');
+  const max = 140;
+  //registering a call back
+  myInput.on("input", function() {
     //Calculating number of characters left
-   let textLength = max-$(this).val().length;
-  //Assigning the value to counter 
+    let textLength = max - $(this).val().length;
+    //Assigning the value to counter
     counter.text(textLength);
-    if($(this).val().length > 140) {
-        counter.css("color", "red");
-        $('#long-tweet').slideDown();
+    if ($(this).val().length > 140) {
+      counter.css("color", "red");
+      $('#long-tweet').slideDown();
+    } else {
+      $('#empty-tweet').slideUp();
+      $('#long-tweet').slideUp();
+      counter.css("color", '#545149');
     }
-    else{
-        $('#empty-tweet').slideUp();
-        $('#long-tweet').slideUp();
-        counter.css("color", "black");
-    }
-});
+  });
 
-})
+});
 
